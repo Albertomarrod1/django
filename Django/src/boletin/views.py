981572902f3e7914e24675db9caaaaa7324c2678
Albertomarrod1 <albertomartinez73.r@gmail.com>
2019-05-20 13:09:32 +0200

    Commit 4 Django
    Este commit abarca los videos 11, 12, 13 y 14. En el video 14, en vez de poner form.cleaned_data, para python 3 en adelante (el que uso) hay que poner form.data, además de siempre usar paréntesis en los print

--- Django/src/boletin/views.py
+++ Django/src/boletin/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from .forms import RegForm
 
 # Create your views here.
 
 def inicio(request):
-	return render(request, 'inicio.html',{})
+	form = RegForm(request.POST or None)
+	if form.is_valid:
+		form_data = form.data
+		print (form_data.get('nombre'))
+		print (form_data.get('edad'))
+	context = {
+		'el_form': form,
+	}
+	return render(request, 'inicio.html',context)
