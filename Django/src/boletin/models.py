3774c27056abe52c7f983c00920504a2e90a7160
Albertomarrod1 <albertomartinez73.r@gmail.com>
2019-05-20 20:47:37 +0200

    Commit 5 Django
    Abarca los videos 15 y 16

--- Django/src/boletin/models.py
+++ Django/src/boletin/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class Registrado(models.Model):
 	nombre=models.CharField(max_length=100, blank=True, null=True)
--- Django/src/boletin/views.py
+++ Django/src/boletin/views.py
@@ -1,15 +1,27 @@
 from django.shortcuts import render
 from .forms import RegForm
-
+from .models import Registrado
 # Create your views here.
-
+"""
 def inicio(request):
 	form = RegForm(request.POST or None)
 	if form.is_valid:
 		form_data = form.data
-		print (form_data.get('nombre'))
-		print (form_data.get('edad'))
+		obj= Registrado.objects.create(email=form_data.get('email'))
 	context = {
 		'el_form': form,
 	}
 	return render(request, 'inicio.html',context)
+"""
+
+def inicio(request):
+	form = RegForm(request.POST or None)
+	if form.is_valid():
+		form_Data = form.data
+		abc = form_Data.get('email')
+		abc2= form_Data.get('nombre')
+		obj = Registrado.objects.create(email=abc, nombre=abc2)
+	context = {
+		'el_form': form,
+	}
+	return render(request, 'inicio.html', context)
